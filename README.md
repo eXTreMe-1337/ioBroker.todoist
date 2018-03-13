cae18e61ea156be4d295d5ea7bb27d53cf54ccfb
eXTreMe-1337 <maxi.balles@googlemail.com>
2018-03-13 19:51:47 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
+![Logo](admin/todoist.png)
 # ioBroker.todoist
-ioBroker todoist.com To-Do-List Adapter for VIS
+
+## Beschreibung / Description
+:de: Dieser Adapter erstellt Datenpunkte mit HTML Code zur Einbindung der To-Do-Listen von todoist.com in VIS mit Hilfe des basic String (unescaped) Widgets. Pro Liste wird ein eigenener Datenpunkt erstellt.
+
+## Einstellungen / Configuration
+### API-Key
+Den notwendigen API Token findet man auf https://todoist.com in den Einstellungen unter dem Punkt Integrationen Die 40stellige Zeichenkette muss hier eingetragen werden.
+
+### Log minimieren
+Um Schreibzugriffe im Log (z.B. auf empfindliche SD-Karten) zu minimieren, kann ein Haken gesetzt werden.
+
+## Aktivierung / Schedule
+Der Adapter startet alle 60sec. 
+
+##  Datenpunkte / Datapoints
+Pro To-Do-Liste wird ein eigener Datenpunkt mit html Code zur Einbindung in VIS mit Hilfe des basic String (unescaped) Widgets angelegt.
+![alt text](todoscriptobjects.png "Datenpunkte")
+
+
+## VIS Nutzung
+Die jeweils erstellten Datenpunkte können in VIS mit dem basic String (unescaped) Widget eingebunden werden. Einfach die den Datenpunkt der jeweiligen Liste bei ObjektID in den Widgeteinstellungen angeben.
+```
+Mit Hilfe von CSS kann die Linkfarbe und die Link-Textdecoration geändert werden. Ein Beispiel:
+```
+
+ /* unvisited link */
+a:link {
+    color: white;
+    text-decoration: none;
+}
+
+/* visited link */
+a:visited {
+    color: white;
+    text-decoration: none;
+}
+
+/* mouse over link */
+a:hover {
+    color: white;
+    text-decoration: none;
+}
+
+/* selected link */
+a:active {
+    color: white;
+    text-decoration: none;
+} 
+```
+
+
+## Changelog
+### 1.0.0 (2018-03-13)
+* (eXTreMe) Adapter created (Vorlage war der Tankerkönig Adapter von pix)
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2018 eXTreMe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,111 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+
+"use strict";
+var utils       = require(__dirname + '/lib/utils'); // Get common adapter utils
+var request     = require('request');
+var lang = 'de';
+
+var adapter = utils.adapter({
+    name:           'todoist',
+    systemConfig:   true,
+    useFormatDate:  true
+});
+
+var optinReset = false;
+var optinNoLog = false;
+
+adapter.on('ready', function () {
+    //adapter.getForeignObject('system.config', function (err, data) {
+        //if (data && data.common) {
+        //    lang  = data.common.language;
+        //}
+
+        writeLog("initializing objects","debug");
+        main();
+    //});
+});
+
+function writeLog(logtext,logtype) { // wenn optinNoLog TRUE keine Ausgabe bei info, warn und debug, nur bei error
+    if (!optinNoLog) { // Ausgabe bei info, debug und error
+        if (logtype === "silly") adapter.log.silly(logtext);
+        if (logtype === "info") adapter.log.info(logtext);
+        if (logtype === "debug") adapter.log.debug(logtext);
+        if (logtype === "warn") adapter.log.warn(logtext);
+        if (logtype === "error") adapter.log.error(logtext);
+    } else { // Ausgabe nur bei error
+        if (logtype === "error") adapter.log.error(logtext);
+    }
+}
+function readData() {
+	var APIprojectsURL = "https://beta.todoist.com/API/v8/projects?token="+adapter.config.apikey;
+	var APItaskURL = "https://beta.todoist.com/API/v8/tasks?token="+adapter.config.apikey;
+  	var request = require("request");
+    var ToDoListen = []; // wird mit IDs der TO-DO Listen befuellt
+    var ToDoListen_names = []; // wird mit Namen der TO-DO Listen befuellt
+    request(APIprojectsURL, function (error, response, body) {
+        var projects_json = JSON.parse(body);
+        for (k = 0; k < projects_json.length; k++) {
+            var projects = parseInt(projects_json[k].id);
+            var projects_name = JSON.stringify(projects_json[k].name);
+            projects_name = projects_name.replace(/\"/g, ""); //entfernt die Anfuehrungszeichen aus dem quellstring
+            ToDoListen[ToDoListen.length] = projects;
+            ToDoListen_names[ToDoListen_names.length] = projects_name;
+            adapter.createState(ToDoListen_names[k], {def: 'false',type: 'string',role: 'html', name: ToDoListen_names[k]+' HTML String'});
+            adapter.log("Datenpunkt "+ToDoListen_names[k]+" erstellt.", "info");
+        }
+    
+    });
+    setTimeout(function() {
+        request(APItaskURL, function (error, response, body) {
+            var json = JSON.parse(body);
+            for (j = 0; j < ToDoListen.length; j++) {
+                var HTMLstring = "";
+                adapter.setState(instanz + pfad + ToDoListen_names[j], "");
+                for (i = 0; i < json.length; i++) {
+                    var Liste = parseInt(json[i].project_id);
+                    var content = JSON.stringify(json[i].content);
+                    content = content.replace(/\"/g, ""); //entfernt die Anfuehrungszeichen aus dem quellstring
+                    content = content[0].toUpperCase() + content.substring(1); // Macht den ersten Buchstaben des strings zu einem Grossbuchstaben
+                    var taskurl = JSON.stringify(json[i].url);
+                    taskurl = taskurl.replace(/\"/g, "");
+                    if (Liste == ToDoListen[j])
+                    {
+                        adapter.log ("["+content+"] in "+ToDoListen_names[j]+" gefunden", "info");
+                        HTMLstring = HTMLstring+"<tr><td><li><a href=\""+taskurl+"\" target=\"_blank\">"+content+"</a></li></td></tr>";
+                        adapter.setState(instanz + pfad + ToDoListen_names[j], "<table><ul>"+HTMLstring+"</ul></table>");
+                    }
+                }
+            }
+    
+      	});
+    }, 5000);
+    adapter.stop();
+}
+
+function readSettings() {
+    //APIKEY
+    writeLog("API Key Länge: " + (adapter.config.apikey?adapter.config.apikey.length:0) + " Zeichen", "debug");
+    if (adapter.config.apikey === undefined) {
+        writeLog("No API-Key found.","error");
+        return; // abbruch
+    } else if (adapter.config.apikey.length < 40) {
+        writeLog("API-Key too short, should be 40 digits.", "error");
+        return; // abbruch
+    } else if (adapter.config.apikey.length > 40) {
+        writeLog("API-Key too long, should be 40 digits.", "error");
+        return; // abbruch
+    } else {
+		readData();
+    }
+    // noLog
+    optinNoLog = adapter.config.noLogs; // wichtig für function writeLog()
+}
+
+function main() {
+    readSettings();
+    setTimeout(function () {
+        writeLog("force terminating adapter after 1 minute", "debug");
+        adapter.stop();
+    }, 60000);
+}
